e6e90c570798adcb32d88b37b4134155bbd2ed3e
ChelseaEvans <chelseaevans@Chelseas-MacBook-Pro.local>
2014-11-15 09:50:36 -0500

    add ping route

--- server.js
+++ server.js
@@ -33,23 +33,29 @@ app.use(express.static(path.join(__dirname, '/public')));
  *
  * ========================================================================= */
 app.get('/', function(req, res) {
-  res.sendfile('views/home.html');
+    res.sendfile('views/home.html');
 });
 
 app.get('/project-runkeeper', function(req, res) {
-  res.sendfile('views/project-runkeeper.html');
+    res.sendfile('views/project-runkeeper.html');
 });
 
 app.get('/project-webmd', function(req, res) {
-  res.sendfile('views/project-webmd.html');
+    res.sendfile('views/project-webmd.html');
 });
 
 app.get('/project-truespirit', function(req, res) {
-  res.sendfile('views/project-truespirit.html');
+    res.sendfile('views/project-truespirit.html');
 });
 
 app.get('/project-timetogo', function(req, res) {
-  res.sendfile('views/project-timetogo.html');
+    res.sendfile('views/project-timetogo.html');
+});
+
+app.get('/ping', function(req, res, next) {
+    return res.json(200, {
+        status: 'ok'
+    });
 });
 
 // var projectController = require('./api/controllers/project');
@@ -57,5 +63,5 @@ app.get('/project-timetogo', function(req, res) {
 
 //start server
 http.createServer(app).listen(PORT, function() {
-  console.log('express server listening on port ' + PORT);
+    console.log('express server listening on port ' + PORT);
 });
